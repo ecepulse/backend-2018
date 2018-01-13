1c6e387a9d051ac9be24b31147d15f733b0b73c7
Jonathan Reynolds <j14reynolds@gmail.com>
2018-01-13 12:53:02 -0600

    Frontend updates

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC9xDuEMC1odH79vyYgAmgblxfPmZfCh48",
+    authDomain: "test-1aa1e.firebaseapp.com",
+    databaseURL: "https://test-1aa1e.firebaseio.com",
+    projectId: "test-1aa1e",
+    storageBucket: "test-1aa1e.appspot.com",
+    messagingSenderId: "728020541050"
+  }
+};
