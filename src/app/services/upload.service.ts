40235a9c23c35c926fa26e6a15dd3779cf180767
Jonathan Reynolds <j14reynolds@gmail.com>
2018-01-14 15:52:26 -0600

    Fix upload service by requiring firebase

--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 
 import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+var firebase = require('firebase');
 
 
 @Injectable()
