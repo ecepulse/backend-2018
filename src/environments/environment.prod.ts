2b44d624f72b2b98035e35fcad48377f7231a5cf
Rahul Rameshbabu <rahulr2@illinois.edu>
2018-05-12 19:26:04 -0500

    Restored production file after pruning out firebase auth credentials from git history.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by rahul_ram on 1/9/18.
+ */
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+  }
+};
